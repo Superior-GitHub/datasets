156c1ff8a4153309e4e302311ad269cb970338c3
Dr. Vural Aksakalli <vaksakalli@gmail.com>
2020-08-07 21:01:06 +1000

    Update prepare_dataset_for_modeling_github.py

--- prepare_dataset_for_modeling_github.py
+++ prepare_dataset_for_modeling_github.py
@@ -1,5 +1,5 @@
 # V. Aksakalli
-# GPL-3.0, 2019
+# GPL-3.0, 2020
 
 import pandas as pd
 import os
@@ -11,26 +11,34 @@ from sklearn.utils import shuffle
 
 
 def prepare_dataset_for_modeling(dataset_name,
+                                 data_directory=None,
                                  n_ins=-1,
                                  random_state=999,
                                  drop_const_columns=True,
+                                 scale_x=True,
                                  is_classification=True):
     """
-    :param dataset_name: name of dataset to be read from github
+    :param dataset_name: name of the dataset (in CSV format)
+    :param data_directory: directory of the dataset. If None, the dataset will be read in from GitHub
     :param n_ins: how many instances to sample (if > 0)
     :param random_state: seed for (1) sampling instances and (2) shuffling instances at the end
     :param drop_const_columns: if True, drop constant-value columns (*after* any sampling)
+    :param scale_x: whether the descriptive features are to be min-max scaled
     :param is_classification: if True, y values will be label-encoded for use in classification models
     :return: x and y NumPy arrays ready for model fitting
     """
 
-    # read in the CSV file into a Pandas data frame
-    if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-            getattr(ssl, '_create_unverified_context', None)):
-        ssl._create_default_https_context = ssl._create_unverified_context
-    github_location = 'https://raw.githubusercontent.com/vaksakalli/datasets/master/'
-    dataset_url = github_location + dataset_name.lower()
-    df = pd.read_csv(io.StringIO(requests.get(dataset_url).content.decode('utf-8')), header=0)
+    if data_directory:
+        # read in from local directory
+        df = pd.read_csv(data_directory + dataset_name, header=0)
+    else:
+        # read in the data file from GitHub into a Pandas data frame
+        if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+                getattr(ssl, '_create_unverified_context', None)):
+            ssl._create_default_https_context = ssl._create_unverified_context
+        github_location = 'https://raw.githubusercontent.com/vaksakalli/datasets/master/'
+        dataset_url = github_location + dataset_name.lower()
+        df = pd.read_csv(io.StringIO(requests.get(dataset_url).content.decode('utf-8')), header=0)
 
     # drop missing values if there are any
     df = df.dropna()
@@ -67,8 +75,9 @@ def prepare_dataset_for_modeling(dataset_name,
     # below, numerical columns will be untouched
     x = pd.get_dummies(x)
 
-    # scale x between 0 and 1
-    x = preprocessing.MinMaxScaler().fit_transform(x)
+    if scale_x:
+        # scale x between 0 and 1
+        x = preprocessing.MinMaxScaler().fit_transform(x)
 
     if is_classification:
         # label-encode y for classification problems
